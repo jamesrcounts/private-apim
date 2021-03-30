457688e57e2dc08d4a46705394b0891ec8cc672e
Jim Counts <jamesrcounts@outlook.com>
2021-03-30 18:12:56 +0000

    Update PFX

--- scripts/certbot.sh
+++ scripts/certbot.sh
@@ -5,7 +5,8 @@ set -euo pipefail
 certbot --version
 
 # Get certificates using Route53 for validation
-DOMAIN="portal.jamesrcounts.com"
+DOMAIN="api.jamesrcounts.com"
 certbot certonly --dns-route53 -d ${DOMAIN}
-mkdir -p ./certs/${DOMAIN}
-cp -R /etc/letsencrypt/archive/${DOMAIN}/* ./certs/${DOMAIN}/
+
+# PFX
+openssl pkcs12 -export -inkey /etc/letsencrypt/live/${DOMAIN}/privkey.pem -in /etc/letsencrypt/live/${DOMAIN}/cert.pem -out ${DOMAIN}.pfx
