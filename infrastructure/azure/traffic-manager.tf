a2bf750ef3a3b6a0b7ac0ae574cb4b76e44d0568
Jim Counts <jamesrcounts@outlook.com>
2021-03-31 03:41:19 +0000

    Add traffic manager

---
+++ infrastructure/azure/traffic-manager.tf
@@ -0,0 +1,29 @@
+
+resource "azurerm_traffic_manager_profile" "tm" {
+  name                = "tm-${local.project}"
+  resource_group_name = azurerm_resource_group.main.name
+  tags = local.tags
+  traffic_routing_method = "Performance"
+
+  dns_config {
+    relative_name = local.project
+    ttl           = 100
+  }
+
+  monitor_config {
+    protocol                     = "https"
+    port                         = 443
+    path                         = "/status-0123456789abcdef"
+    interval_in_seconds          = 30
+    timeout_in_seconds           = 9
+    tolerated_number_of_failures = 3
+  }
+}
+
+resource "azurerm_traffic_manager_endpoint" "agw" {
+  name                = "tm-ep-${local.project}"
+  profile_name        = azurerm_traffic_manager_profile.tm.name
+  resource_group_name = azurerm_resource_group.main.name
+  target_resource_id = azurerm_public_ip.agw.id
+  type                = "azureEndpoints"
+}
